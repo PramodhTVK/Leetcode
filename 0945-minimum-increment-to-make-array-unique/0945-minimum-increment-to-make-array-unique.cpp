2d96fdf688851909c1626f814afe8680942432cf
Pramodh <95704815+PramodhTVK@users.noreply.github.com>
2022-12-01 19:30:15 +0530

    Time: 393 ms (50.73%), Space: 65.7 MB (36.05%) - LeetHub

---
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int ans=0,need=0;
+        for(auto it:nums){
+            ans+=max(need-it,0);
+            need=max(it,need)+1;
+        }
+        return ans;
+    }
+};
