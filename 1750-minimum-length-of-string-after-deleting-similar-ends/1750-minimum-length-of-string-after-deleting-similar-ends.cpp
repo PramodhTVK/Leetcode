b9e2a6afcc75b06d6ae65d25f87ca051c123dac9
Pramodh <95704815+PramodhTVK@users.noreply.github.com>
2022-12-14 19:35:13 +0530

    Time: 26 ms (86.70%), Space: 12.9 MB (14.16%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -4,11 +4,14 @@ public:
         int n=s.length();
         int low=0;
         int high=n-1;
-        char ch=s[0];
-        while(low<high && s[low]==s[high]){
-            while(low<=high && s[low]==ch)low++;
-            while(low<=high && s[high]==ch)high--;
-            ch=s[low];
+        while(low<high){
+            if(s[low]==s[high]){
+           char ch=s[low];
+           while(low<=high && s[low]==ch)low++;
+           while(low<=high && s[high]==ch)high--;}
+            else{
+                return high-low+1;
+            }
         }
         return high-low+1;
     }
