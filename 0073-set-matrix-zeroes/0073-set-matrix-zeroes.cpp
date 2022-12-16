bbeb680a99c652737689ae3c4ef1470040f90739
Pramodh <95704815+PramodhTVK@users.noreply.github.com>
2022-12-16 13:06:24 +0530

    Time: 16 ms (89.09%), Space: 13.3 MB (56.26%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -17,7 +17,8 @@ public:
         }
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
-                if(matrix[i][0]==0 || matrix[0][j]==0)matrix[i][j]=0;
+                if(matrix[i][0]==0)matrix[i][j]=0;
+                if(matrix[0][j]==0)matrix[i][j]=0;
                
             }
         }
