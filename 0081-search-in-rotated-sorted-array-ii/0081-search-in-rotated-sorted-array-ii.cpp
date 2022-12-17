6fca778e30ef07a23c3ff556cd97c57e9627d96d
Pramodh <95704815+PramodhTVK@users.noreply.github.com>
2022-12-17 12:17:23 +0530

    Time: 3 ms (98.13%), Space: 14.1 MB (45.45%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -5,23 +5,20 @@ public:
         int low=0;
         int high=n-1;
         while(low<=high){
-            int mid=(low+high)/2;
-        while(low<mid && nums[low]==nums[low+1])low++;
-        while(high>mid && nums[high]==nums[high-1])high--;
-            mid=(low+high)/2;
+            int mid=(high+low)/2;
+            while(low<mid && nums[low]==nums[low+1])low++;
+            while(high>mid && nums[high]==nums[high-1])high--;
+            mid=(high+low)/2;
             if(nums[mid]==target)return true;
             else if(nums[mid]>=nums[low]){
-                if(nums[low]<=target && target<=nums[mid])
-                    high=mid-1;
-                else 
-                    low=mid+1;
+                if(nums[low]<=target && target<=nums[mid])high=mid-1;
+                else low=mid+1;
             }
             else{
-                if(nums[mid]<=target && target<=nums[high])
-                    low=mid+1;
-                else
-                    high=mid-1;
-            }}
+                if(nums[mid]<=target && target<=nums[high])low=mid+1;
+                else high=mid-1;
+            }
+        }
         return false;
     }
 };
