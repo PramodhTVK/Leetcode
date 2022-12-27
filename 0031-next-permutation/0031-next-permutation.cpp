1ad114dc09d1328128c9d5adc5fde0089b1eaadb
Pramodh <95704815+PramodhTVK@users.noreply.github.com>
2022-12-27 22:36:54 +0530

    Time: 7 ms (61.14%), Space: 12.2 MB (37.74%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -7,14 +7,12 @@ public:
             if(nums[i]<nums[i+1])
                 break;
         }
-        if(i<0){reverse(nums.begin(),nums.end());}
-        else{
-        for(j=n-1;j>=0;j--){
+        if(i<0)reverse(nums.begin(),nums.end());
+        else{for(j=n-1;j>=0;j--){
             if(nums[j]>nums[i])
-              break;
+                break;
         }
         swap(nums[i],nums[j]);
         reverse(nums.begin()+i+1,nums.end());}
-        
     }
 };
