aab1cb8db783f272752543468fe6d18eb601f5b6
Pramodh <95704815+PramodhTVK@users.noreply.github.com>
2023-01-02 22:45:36 +0530

    Added solution - LeetHub

---
+++ Count Digits - GFG/count-digits.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int N){
+        int count=0;
+        int ans=N;
+        while(N){
+            int res=N%10;
+            if(res>0 && ans%res==0)count++;
+            N/=10;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
